6206461b6a5b11885114926178abd49564d2984a
356573574 <356573574@qq.com>
2024-04-26 09:55:08 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,53 @@
+var config = {
+    texts: [
+        "送给我",
+        "心爱的姑娘",
+        "今天是你的生日",
+        "这是我们相识以来的",
+        "第十五个生日！",
+        "这些年来，我见过你",
+        "青春的样子",
+        "性感的样子",
+        "活泼的样子",
+        "都是我喜欢的样子",
+        "对了，礼物收全了吗？",
+        "今年要吃好好的",
+        "喂饱饱的",
+        "然后才有力气干活",
+        "和想我！",
+        "生日快乐~~",
+    ],
+
+    imgs: {
+        "送给我": "./imgs/songxinxin.jpeg",
+        "心爱的姑娘": "./imgs/nvhai.jpg",
+        "今天是你的生日": "./imgs/shengri1.jpeg",
+        "这是我们相识以来的": "./imgs/zaiyiqi.png",
+        "第十五个生日！": "./imgs/shengri2.jpg",
+        "这些年来，我见过你": "./imgs/yangzi.jpeg",
+        "青春的样子": "./imgs/xy2.jpg",
+        "性感的样子": "./imgs/xy3.jpg",
+        "活泼的样子": "./imgs/xy4.png",
+        "都是我喜欢的样子": "./imgs/xy1.jpg",
+        "对了，礼物收全了吗？": "./imgs/chuo.gif",
+        "今年要吃好好的": "./imgs/chihaodian.jpeg",
+        "喂饱饱的": "./imgs/weibao.jpeg",
+        "然后才有力气干活": "./imgs/gz.gif",
+        "和想我！": "./imgs/xw.gif",
+        "生日快乐~~": "./imgs/bd1.jpg"
+    },
+    
+    desc: {
+        turn_on: "点我开始",
+        play: "上音乐",
+        bannar_coming: "搞点颜色",
+        balloons_flying: "好像还少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐～",
+        story: "A MESSAGE FOR YOU"
+    },
+
+    // 结束语
+    loveText: '爱你的杨先生~'
+};
